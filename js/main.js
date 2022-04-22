4f26abbe4d3cf9504a2f71bf6262c6610b5456f3
Eric <coolo_ricky@hotmail.com>
2022-04-22 12:31:11 -0400

    did a lot of work, need to finish emailjs, not all is responsive like I would want it to, media queries need to be added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-function hamburgerMenu(){
+function sendMail(params) {
+    var tempParams ={
+        from_name: document.getElementById('fname').value,
+        to_name: "Eric",
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        language: document.getElementById('language').value,
+    };
 
-}
-
-function SaveFormData(){
-
-}
-
-function emailValidation(){
-    
+    emailsjs.send('service_u26rjlm', 'template_1029ss1', tempParams)
+    .then(function(res) {
+        console.log("success", res.status);
+    })
 }
