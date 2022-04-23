69e73a9d70060ddd9b9cdd00b0ae09b91f628b04
Eric <coolo_ricky@hotmail.com>
2022-04-22 23:11:57 -0400

    lots of things broke at the end, I lost my desktop code, only 1 media query works 480 screen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-function sendMail(params) {
+function sendMail(event) {
+    event.preventDefault();
     var tempParams ={
         from_name: document.getElementById('fname').value,
         to_name: "Eric",
@@ -6,9 +7,13 @@ function sendMail(params) {
         phone: document.getElementById('phone').value,
         language: document.getElementById('language').value,
     };
-
-    emailsjs.send('service_u26rjlm', 'template_1029ss1', tempParams)
-    .then(function(res) {
+    try{
+        emailjs.send('service_u26rjlm', 'template_1029ss1', tempParams)
+        .then(function(res) {
         console.log("success", res.status);
-    })
-}
+     })
+    } catch(e) {
+      console.log(e);
+    }
+  }
+document.getElementById('submit-button').addEventListener('click', sendMail)
